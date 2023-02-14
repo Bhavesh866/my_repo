dd7c17583e92dbfa64d42c7b1ef6654acf32e447
Bhavesh <prathmesh.adbal@finaryatech.com>
2023-02-14 17:30:22 +0530

    f

---
+++ src/test/java/com/tms/tmsapi/TmsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.tmsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
